7fab6cae9681e5f6be305fcbbdbbe73880c8d275
Sally <sme418@nyu.edu>
2025-03-24 18:33:33 -0400

    updated App.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// import { useEffect, useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Styling & Components
+import "./App.css";
+
+// Main Pages
+import Home from "./pages/Home";
+import Primordia from "./pages/Primordia";
+import Noctilum from "./pages/Noctilum";
+import Oblivia from "./pages/Oblivia";
+import Sylvalum from "./pages/Sylvalum";
+import Cauldros from "./pages/Cauldros";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      // home page
+      path: "/",
+      element: <Home />,
+    },
+    {
+      // Primordia page
+      path: "/primordia",
+      element: <Primordia />,
+    },
+    {
+      // Noctilum page
+      path: "/noctilum",
+      element: <Noctilum />,
+    },
+    {
+      // Oblivia page
+      path: "/oblivia",
+      element: <Oblivia />,
+    },
+    {
+      // Sylvalum page
+      path: "/sylvalum",
+      element: <Sylvalum />,
+    },
+    {
+      // Cauldros page
+      path: "/cauldros",
+      element: <Cauldros />,
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
